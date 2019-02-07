0ce0572689d33445bbe54d3f73a19e00292ee454
Your Name <siddhrathyadav1999@gmail.com>
2019-02-07 21:56:51 +0530

    homepage image added

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ $(document).ready( function() {
     if ($('.element').length) {
         $('.element').each(function () {
             $(this).typed({
-                strings: [$(this).data('text1'), $(this).data('text2'), $(this).data('text3'), $(this).data('text4'), $(this).data('text5'), $(this).data('text6'), $(this).data('text7')], 
+                strings: [$(this).data('text1'), $(this).data('text2')], 
                 loop: $(this).data('loop') ? $(this).data('loop') : false ,
                 backDelay: $(this).data('backdelay') ? $(this).data('backdelay') : 2000 ,                
                 typeSpeed: 10,
