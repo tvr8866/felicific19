5cd85df60d91d0bb156a5dae17b98412a9db9f02
anmolsaxena10 <anmol.saxena10@gmail.com>
2019-02-23 01:47:41 +0530

    minor changes

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ $(document).ready( function() {
     if ($('.element').length) {
         $('.element').each(function () {
             $(this).typed({
-                strings: [$(this).data('text1'), $(this).data('text2')], 
+                strings: [$(this).data('text1'), $(this).data('text2'), $(this).data('text3')], 
                 loop: $(this).data('loop') ? $(this).data('loop') : false ,
                 backDelay: $(this).data('backdelay') ? $(this).data('backdelay') : 2000 ,                
                 typeSpeed: 10,
